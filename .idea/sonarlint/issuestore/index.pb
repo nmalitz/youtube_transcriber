c6619798f44c42ee8d67fb80b9b8a086672b6c1d
Nathan Malitz <namalitz@bsu.edu>
2021-02-27 13:23:02 -0500

    Init commit.

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+J
+download_youtube_as_mp3.py,a/9/a9b59a43308b56973ef561d463fe1ff3e1a5e480
---
+++ main.py
@@ -0,0 +1,72 @@
+from __future__ import unicode_literals
+import youtube_dl
+import math
+import contextlib
+import wave
+import speech_recognition as sr
+
+
+def main():
+    url = "https://www.youtube.com/watch?v=Sm3Wye6uKXQ&t=23s"
+    response, vid_id = download_youtube_as_mp3(url)
+    make_transcript(response, vid_id)
+
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
+
+
+def download_youtube_as_mp3(url):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        # 'outtmpl': './audio_output/%(title)s.%(ext)s',
+        'outtmpl': './audio_output/1.%(ext)s',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'wav',
+            'preferredquality': '192',
+        }],
+        'progress_hooks': [my_hook],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+        info_dict = ydl.extract_info(url, download=False)
+        video_title = info_dict.get('title', None)
+        video_title = video_title.replace(":", "-")
+        video_title = video_title.replace("**", "_")
+        video_id = info_dict.get("id", None)
+        return video_title, video_id
+
+
+def make_transcript(title, vid_id):
+    fp = "./audio_output/1.wav"
+
+    duration = get_duration(fp)
+    total_duration = math.ceil(duration / 60)
+
+    r = sr.Recognizer()
+    f = open(f"./transcript/{title}.txt", "a")
+    f.write("Title: " + title + "\n")
+    f.write("ID: " + vid_id + "\n")
+    f.write("Transcript:\n")
+    for i in range(0, total_duration):
+        with sr.AudioFile(fp) as source:
+            r.adjust_for_ambient_noise(source)
+            audio = r.record(source, offset=i * 60, duration=60)
+        
+        f.write(r.recognize_google(audio))
+        f.write(" ")
+    f.close()
+
+
+def get_duration(file_path):
+    with contextlib.closing(wave.open(file_path, "r")) as f:
+        frames = f.getnframes()
+        rate = f.getframerate()
+        duration = frames / float(rate)
+        return duration
+
+
+main()
